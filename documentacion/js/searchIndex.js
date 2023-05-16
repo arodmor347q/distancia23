241ab284a17ba11803fa6b213314cec8c1b5795b
arodmor347q <133794680+arodmor347q@users.noreply.github.com>
2023-05-16 18:48:57 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\suma\u0028\u0029",
+            "name": "suma",
+            "summary": "Esta\u0020funcion\u0020devuelve\u0020la\u0020suma\u0020de\u0020los\u0020argumentos\nenviados\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_suma"
+        },                {
+            "fqsen": "\\resta\u0028\u0029",
+            "name": "resta",
+            "summary": "Esta\u0020funcion\u0020devuelve\u0020la\u0020resta\u0020de\u0020los\u0020argumentos\nenviados\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_resta"
+        },                {
+            "fqsen": "\\saluda\u0028\u0029",
+            "name": "saluda",
+            "summary": "Esta\u0020funcion\u0020devuelve\u0020un\u0020saludo\u0020con\u0020el\u0020nombre\u0020que\u0020le\nenviamos\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_saluda"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
